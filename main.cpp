373d55a14d8f8b5a05744cbe6b8b7dba5241737d
PGStudioGH <80467798+PGStudioGH@users.noreply.github.com>
2023-08-03 14:54:48 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <DigmeBox.h>
+
+class Game : public DigmeBox
+{
+public:
+    Game()
+    {
+        NameApp = "New window";
+    };
+    ~Game() {};
+
+public:
+    bool create() override
+    {
+        max_fps = 0;
+        DB_setWindowTitle("Super app!");
+
+        image.loadSprFromImage("test.png");
+        DB_initSprGC(&image);
+
+        fps.create(13, 1);
+        fps << "Честь ордена!";
+        fps.update();
+        DB_initTextGC(&fps);
+
+        return true;
+    }
+    bool update() override
+    {
+        DB_fill(C_GREEN);
+
+        DB_drawDecalSprite(8, 8, &image);
+        DB_drawDecalText(64, 24, &fps);
+        
+        return true;
+    }
+private:
+    int max_fps;
+    SprGC image;
+    TextGC fps;
+};
+
+DIGMEBOX_MAIN()
+{
+    Game game;
+    game.construct(1024, 512, 256, 128, DB_HARDWARE_ACCELERATED);
+    game.start();
+
+    return 0;
+}
